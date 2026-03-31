54e3ece1754a3f947420c26363809be87b8d4fb3
A.Sravani <2410040126@klh.edu.in>
2026-04-01 03:59:27 +0530

    port fix

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://localhost:8080/api";
+export const API_BASE = "https://chronosync-docker.onrender.com/api";
 
 // // Helper to get auth headers
 // export const getAuthHeaders = () => {
